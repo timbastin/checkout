282283cadfc4566d4ee887237f00f1d7ab9014b3
Tim Bastin <38261809+timbastin@users.noreply.github.com>
2024-10-30 12:42:13 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@
 
 "use strict";
 
+console.log("CUSTOM ACTION LOG")
+
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
